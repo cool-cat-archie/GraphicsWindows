139f6c2968128bc173e91f13fbb47db0c24ec175
Archie Menon <architmenon.mnn@gmail.com>
2022-04-25 18:09:37 -0600

    eliminated some dead code

--- GraphicsWindows/Source.cpp
+++ GraphicsWindows/Source.cpp
@@ -18,16 +18,13 @@ using namespace cy;
 cy::GLTextureCubeMap envmap;
 
 
-GLuint object_buffer;
 GLuint plane_buffer;
 GLuint enviro_buffer;
 GLuint object_vao;
 GLuint plane_vao;
 GLuint enviro_vao;
-TriMesh tm;
 
 GLuint program;
-GLSLProgram object_prog;
 GLSLProgram plane_prog;
 GLSLProgram stochastic_prog;
 GLSLProgram hashed_prog;
@@ -36,20 +33,10 @@ GLSLProgram enviro_prog;
 int totalVerts;
 int cubeVerts;
 Vec3f omega = Vec3f(-1.0f, 1.0f, 1.0f);
-cy::GLRenderTexture2D renderBuffer;
 
-cyGLTexture2D diffuse_tex;
-cyGLTexture2D spec_tex;
 cyGLTexture2D tree_tex;
 cyGLTexture2D undistributed;
 
-Matrix4f object_mvp;
-Matrix4f object_mv;
-Matrix3f object_inverseTranspose;
-float object_Ns;
-//Vec3f texture_inKa;
-//Vec3f texture_inKs;
-//Vec3f texture_inKd;
 Matrix4f plane_mvp;
 
 int textureWidth;
@@ -197,13 +184,9 @@ void myMouseMotion(int x, int y) {
 		int mod = glutGetModifiers();
 		if (mod == GLUT_ACTIVE_CTRL) {
 			int diffVert = y - oldVertPlane;
-			fovPlane -= /*(45.0 / float(glutGet(GLUT_SCREEN_HEIGHT))) */ diffVert;
+			fovPlane -= diffVert;
 			if (fovPlane < 1)
 				fovPlane = 1;
-			/*if (fovPlane > 45)
-				fovPlane = 45;*/
-				/*cy::Matrix4f rotMatrix =
-					cy::Matrix4f::RotationX(rotationX) * cy::Matrix4f::RotationY(rotationY);*/
 
 			cy::Matrix4f view = cy::Matrix4f::View(Vec3f(0.0f, 1.0f, 3.0f),
 				Vec3f(0.0f, 0.0f, 0.0f),
@@ -265,86 +248,14 @@ void myMouseMotion(int x, int y) {
 					float(glutGet(GLUT_SCREEN_WIDTH)) / float(glutGet(GLUT_SCREEN_HEIGHT)),
 					0.1f,
 					1000.0f);
-			/* plane_mvp = projMatrix * view * rotMatrix;
-			 plane_prog["mvp"] = plane_mvp;
-			 plane_prog["mv"] = view * rotMatrix;*/
 
 			 //same for environment map
 			enviro_prog["mvp"] = projMatrix * view;
 			enviro_prog["rot"] = rotMatrix;
 
-
-			/*Matrix3f invTrans = Matrix3f(view * rotMatrix);
-			invTrans.Invert();
-			invTrans.Transpose();
-			plane_prog["inverseTranspose"] = invTrans;*/
 		}
 	}
-	else if (mouseButton == GLUT_RIGHT_BUTTON) {
-		int mod = glutGetModifiers();
-		if (mod == GLUT_ACTIVE_ALT) {
-			int diffVert = y - oldVertPlane;
-			fovPlane -= (45.0 / float(glutGet(GLUT_SCREEN_HEIGHT))) * diffVert;
-			if (fovPlane < 1)
-				fovPlane = 1;
-			if (fovPlane > 45)
-				fovPlane = 45;
-			cy::Matrix4f rotMatrix =
-				cy::Matrix4f::RotationY(rotationY) * cy::Matrix4f::RotationX(rotationX);
-
-			cy::Matrix4f view = cy::Matrix4f::View(Vec3f(0.0f, 1.0f, 3.0f),
-				Vec3f(0.0f, 0.0f, 0.0f),
-				Vec3f(0.0f, 1.0f, 0.0f));
-			cy::Matrix4f projMatrix =
-				cy::Matrix4f::Perspective(
-					DEG2RAD(fovPlane),
-					float(glutGet(GLUT_SCREEN_WIDTH)) / float(glutGet(GLUT_SCREEN_HEIGHT)),
-					0.1f,
-					1000.0f);
-			plane_mvp = projMatrix * view * rotMatrix;
-			plane_prog["mvp"] = plane_mvp;
-			stochastic_prog["mvp"] = plane_mvp;
-			hashed_prog["mvp"] = plane_mvp;
-			oldVertPlane = y;
-
-		}
-		else {
-			/* static int oldVertText;
-			 int diffVert = y - oldVertText;
-			 fovText -= (45.0 / float(glutGet(GLUT_SCREEN_HEIGHT))) * diffVert;
-			 if (fovText < 1)
-				 fovText = 1;
-			 if (fovText > 45)
-				 fovText = 45;
-			 cy::Matrix4f rotMatrix =
-				 cy::Matrix4f::RotationY(rotationY) * cy::Matrix4f::RotationX(rotationX);
-
-			 cy::Matrix4f view = cy::Matrix4f::View(Vec3f(0.0f, 1.0f, 3.0f),
-				 Vec3f(0.0f, 0.0f, 0.0f),
-				 Vec3f(0.0f, 1.0f, 0.0f));
-			 cy::Matrix4f projMatrix =
-				 cy::Matrix4f::Perspective(
-					 DEG2RAD(fovText),
-					 float(glutGet(GLUT_SCREEN_WIDTH)) / float(glutGet(GLUT_SCREEN_HEIGHT)),
-					 0.1f,
-					 1000.0f);
-			 tm.ComputeBoundingBox();
-			 assert(tm.IsBoundBoxReady());
-			 Vec3f min = tm.GetBoundMin();
-			 Vec3f max = tm.GetBoundMax();
-			 Vec3f mid = -(min + max) / 2.0f;
-			 cy::Matrix4f trans = cy::Matrix4f::Translation(mid * 0.03);
-			 object_mvp = projMatrix * view * rotMatrix * trans;
-			 object_prog["mvp"] = object_mvp;
-
-			 oldVertText = y;
-
-			 Matrix3f inverseTranspose = Matrix3f(view * rotMatrix * trans);
-			 inverseTranspose.Invert();
-			 inverseTranspose.Transpose();
-			 object_prog["inverseTranspose"] = inverseTranspose;*/
-		}
-	}
+	
 	oldX = x;
 	oldY = y;
 	glutPostRedisplay();
@@ -363,8 +274,7 @@ std::pair<int, int> sendTextureToFragmentShader(cyGLTexture2D* tex, std::string 
 
 	tex->SetImage(image.data(), 4, width, height);
 	tex->BuildMipmaps();
-	//tex->Bind(texUnit);
-	//plane_prog[varName.data()] = texUnit;
+
 	return { width, height };
 }
 
@@ -378,13 +288,6 @@ void createPlane() {
 	vertexes.push_back(Vec3f(-1, -1, 0));
 	vertexes.push_back(Vec3f(1, -1, 0));
 
-	//vertexes.push_back(Vec3f(-2, -.6, 2));
-	//vertexes.push_back(Vec3f(2, -.6, 2));
-	//vertexes.push_back(Vec3f(-2, -.6, -2));
-
-	//vertexes.push_back(Vec3f(2, -.6, 2));
-	//vertexes.push_back(Vec3f(-2, -.6, -2));
-	//vertexes.push_back(Vec3f(2, -.6, -2));
 
 	Vec3f* planeVerts = vertexes.data();
 
@@ -612,8 +515,6 @@ void AlphaDistribute() {
 
 int main(int argc, char** argv) {
 	char* objName = argv[1];
-	/*std::string mtlName(objName);
-	mtlName.replace(mtlName.length() - 4, 4, ".mtl");*/
 
 	glutInit(&argc, argv);
 
@@ -642,53 +543,12 @@ int main(int argc, char** argv) {
 	glBindVertexArray(object_vao);
 
 
-	tm.LoadFromFileObj(objName);
-	std::vector<Vec3f> triangleVerts;
-
-	//iterate through the faces
-	for (int i = 0; i < tm.NF(); i++) {
-		cy::TriMesh::TriFace  face = tm.F(i);
-		cy::TriMesh::TriFace  faceText = tm.FT(i);
-
-		//face.v[?] gives the index of some vertex
-		triangleVerts.push_back(tm.V(face.v[0]));
-		triangleVerts.push_back(tm.VN(face.v[0]));
-		triangleVerts.push_back(tm.VT(faceText.v[0]));
-
-		triangleVerts.push_back(tm.V(face.v[1]));
-		triangleVerts.push_back(tm.VN(face.v[1]));
-		triangleVerts.push_back(tm.VT(faceText.v[1]));
-
-		triangleVerts.push_back(tm.V(face.v[2]));
-		triangleVerts.push_back(tm.VN(face.v[2]));
-		triangleVerts.push_back(tm.VT(faceText.v[2]));
-
-		totalVerts += 3;
-	}
-	Vec3f* verts = triangleVerts.data();
-
-	glGenBuffers(1, &object_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, object_buffer);
-	glBufferData(GL_ARRAY_BUFFER,
-		sizeof(Vec3f) * totalVerts * 3,
-		verts,
-		GL_STATIC_DRAW);
-
-	//compile the shaders
-	object_prog.BuildFiles("shader.vert",
-		"shader.frag");
 	plane_prog.BuildFiles("vertexPlane.vert",
 		"fragmentPlane.frag");
 	stochastic_prog.BuildFiles("vertexPlane.vert",
 		"stochastic.frag");
 	hashed_prog.BuildFiles("vertexPlane.vert",
 		"hashed.frag");
-
-
-
-	object_prog.Bind();
-	program = object_prog.GetID();
-
 
 	cy::Matrix4f view = cy::Matrix4f::View(Vec3f(0.0f, 0.0f, 3.0f),
 		Vec3f(0.0f, 0.0f, 0.0f),
@@ -699,56 +559,13 @@ int main(int argc, char** argv) {
 			float(glutGet(GLUT_SCREEN_WIDTH)) / float(glutGet(GLUT_SCREEN_HEIGHT)),
 			0.1f,
 			1000.0f);
-	tm.ComputeBoundingBox();
-	assert(tm.IsBoundBoxReady());
-	Vec3f min = tm.GetBoundMin();
-	Vec3f max = tm.GetBoundMax();
-	Vec3f mid = -(min + max) / 2.0f;
-	cy::Matrix4f trans = cy::Matrix4f::Translation(mid * 0.05);
+	
 
-	object_mvp = projMatrix * view * trans;
-	object_prog["mvp"] = object_mvp;
-	object_mv = view * trans;
-	object_prog["mv"] = object_mv;
 
 	//set mv for plane as well
 	plane_prog["mv"] = view;
 	stochastic_prog["mv"] = view;
 	hashed_prog["mv"] = view;
-
-	object_prog["rot"] = object_mvp.Identity();
-	plane_prog["rot"] = object_mvp.Identity();
-	stochastic_prog["rot"] = object_mvp.Identity();
-	hashed_prog["rot"] = object_mvp.Identity();
-
-
-	object_inverseTranspose = Matrix3f(view);
-	object_inverseTranspose.Invert();
-	object_inverseTranspose.Transpose();
-	object_prog["inverseTranspose"] = object_inverseTranspose;
-
-	GLuint pos = glGetAttribLocation(
-		program, "pos");
-	glEnableVertexAttribArray(pos);
-	glVertexAttribPointer(
-		pos, 3, GL_FLOAT,
-		GL_FALSE, sizeof(Vec3f) * 3, (GLvoid*)0);
-	GLuint clr = glGetAttribLocation(
-		program, "clr");
-	glEnableVertexAttribArray(clr);
-	glVertexAttribPointer(
-		clr, 3, GL_FLOAT,
-		GL_FALSE, sizeof(Vec3f) * 3, (GLvoid*)(sizeof(Vec3f)));
-	GLuint txc = glGetAttribLocation(
-		program, "txc");
-	glEnableVertexAttribArray(txc);
-	glVertexAttribPointer(
-		txc, 3, GL_FLOAT,
-		GL_FALSE, sizeof(Vec3f) * 3, (GLvoid*)(2 * sizeof(Vec3f)));
-
-	object_prog["Ns"] = 20;
-	object_prog["omega"] = omega;
-
 
 	createPlane();
 	std::pair<int, int> dimensions = sendTextureToFragmentShader(&tree_tex, "LushPine.png", "tree", DISTRIBUTED_TEX_UNIT);
--- GraphicsWindows/fragmentPlane.frag
+++ GraphicsWindows/fragmentPlane.frag
@@ -3,7 +3,6 @@
 layout(location = 0) out vec4 color;
 uniform sampler2D tree;
 in vec4 v;
-uniform mat4 rot; //transform the view by the rotation
 
 in vec2 texCoord;
 
